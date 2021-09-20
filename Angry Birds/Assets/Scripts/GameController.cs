9b69893dc07df3a5f7c23044b27706d6ff6c5522
thellost <aldy10ball@gmail.com>
2021-09-21 00:42:05 +0700

    add enemy interaction and health

--- Angry Birds/Assets/Scripts/GameController.cs
+++ Angry Birds/Assets/Scripts/GameController.cs
@@ -6,7 +6,9 @@ public class GameController : MonoBehaviour
 {
     public SlingShooter SlingShooter;
     public List<Bird> Birds;
+    public List<Enemy> Enemies;
 
+    private bool _isGameEnded = false;
     void Start()
     {
         for (int i = 0; i < Birds.Count; i++)
@@ -14,13 +16,41 @@ public class GameController : MonoBehaviour
             //object game controller akan meng "subscribe" fungsi onbirddestroyed , dan menjalankan fungsi Change bird
             Birds[i].OnBirdDestroyed += ChangeBird;
         }
+
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
         SlingShooter.InitiateBird(Birds[0]);
     }
     public void ChangeBird()
     {
+        if (_isGameEnded)
+        {
+            return;
+        }
+
         Birds.RemoveAt(0);
 
         if (Birds.Count > 0)
             SlingShooter.InitiateBird(Birds[0]);
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (Enemies.Count == 0)
+        {
+            _isGameEnded = true;
+        }
     }
 }
