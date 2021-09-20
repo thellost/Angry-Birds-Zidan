aa33c00d1dcd5f81fbaa21fbe7739abb973febe1
thellost <aldy10ball@gmail.com>
2021-09-20 23:41:26 +0700

    add throw and game controller

---
+++ Angry Birds/Assets/Scripts/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public List<Bird> Birds;
+
+    void Start()
+    {
+        SlingShooter.InitiateBird(Birds[0]);
+    }
+}
